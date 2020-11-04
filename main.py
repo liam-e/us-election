32caa24eb7cfa60abc2cf0a8ed2835fb8170730e
Liam Ensbey <ensbeyliam@myvuw.ac.nz>
2020-11-05 12:54:01 +1300

    Changed url, added log file, works in both windows/linux

--- main.py
+++ main.py
@@ -1,54 +1,64 @@
 from selenium import webdriver
 import json
 from datetime import datetime
+import platform
 
-options = webdriver.ChromeOptions()
-options.add_argument('headless')
-options.add_argument('--no-sandbox')
-options.add_argument('--disable-dev-shm-usage')
-options.add_argument('--disable-gpu')
-options.add_argument('--remote-debugging-port=9222')
 
-# driver = webdriver.Chrome(executable_path="/usr/local/bin/chromedriver", options=options)
+def update_colleges():
 
-driver = webdriver.Chrome(options=options)
+	datetime_string = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
 
-url = "https://www.google.com/search?q=us+election&oq=us+election&aqs=chrome.0.69i59l4j69i60l3.1961j0j1&sourceid=chrome&ie=UTF-8"
+	options = webdriver.ChromeOptions()
+	options.add_argument('headless')
+	options.add_argument('--no-sandbox')
+	options.add_argument('--disable-dev-shm-usage')
+	options.add_argument('--disable-gpu')
+	options.add_argument('--remote-debugging-port=9222')
 
-driver.get(url)
+	if platform.system() == "Windows":
+		driver = webdriver.Chrome(options=options)
+	else:
+		driver = webdriver.Chrome(executable_path="/usr/local/bin/chromedriver", options=options)
 
-biden_xpath = '/html/body/div[7]/div[2]/div[10]/div[1]/div[2]/div/div[2]/div[2]/div/div/div[1]/div/div/div[1]/div/div/div[2]/div/div[2]/div/div[2]/div[1]/div/div[2]/div/div/div[4]/div/div[1]/div[1]/div/div[2]/span'
-trump_xpath = '/html/body/div[7]/div[2]/div[10]/div[1]/div[2]/div/div[2]/div[2]/div/div/div[1]/div/div/div[1]/div/div/div[2]/div/div[2]/div/div[2]/div[1]/div/div[2]/div/div/div[4]/div/div[1]/div[3]/div/div[2]/span'
-trump_count = None
-biden_count = None
+	url = "https://www.theguardian.com/us-news/ng-interactive/2020/nov/03/us-election-2020-live-results-donald-trump-joe-biden-who-won-presidential-republican-democrat"
 
-try:
-	biden_count = int(driver.find_element_by_xpath(biden_xpath).text.strip())
-except ValueError:
-	print(driver.find_element_by_xpath(biden_xpath).text.strip())
+	driver.get(url)
 
-try:
-	trump_count = int(driver.find_element_by_xpath(trump_xpath).text.strip())
-except ValueError:
-	print(driver.find_element_by_xpath(trump_xpath).text.strip())
+	biden_xpath = '/html/body/div[4]/article/div/div[2]/div/figure/figure/div/div/div[1]/div/div[1]/div[1]/div[2]/div[1]/div[1]'
+	trump_xpath = '/html/body/div[4]/article/div/div[2]/div/figure/figure/div/div/div[1]/div/div[1]/div[2]/div[2]/div[2]/div[2]'
 
-if biden_count and trump_count:
+	try:
+		biden_count = int(driver.find_element_by_xpath(biden_xpath).text.strip())
+		trump_count = int(driver.find_element_by_xpath(trump_xpath).text.strip())
+	except ValueError:
+		with open("log.txt", "a") as f:
+			f.write(f"Failure ValueError {datetime_string}\n")
+			return
 
-	data = {
-		"biden": {
-			"college_count": f"{biden_count}",
-			"width": f"{round(biden_count/270*50, 2)}%"
-		},
-		"trump": {
-			"college_count": f"{trump_count}",
-			"width": f"{round(trump_count/270*50, 2)}%"
-		},
-		"time_updated": datetime.now().strftime("%d/%m/%Y %H:%M:%S")
-	}
+	if biden_count and trump_count:
 
-	with open("public_html/us-election/data.json", "w") as f:
-		json.dump(data, f, indent=4)
+		data = {
+			"biden": {
+				"college_count": f"{biden_count}",
+				"width": f"{round(biden_count/270*50, 2)}%"
+			},
+			"trump": {
+				"college_count": f"{trump_count}",
+				"width": f"{round(trump_count/270*50, 2)}%"
+			},
+			"time_updated": datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+		}
 
-driver.close()
+		with open("public_html/us-election/data.json", "w") as f:
+			json.dump(data, f, indent=4)
 
-driver.quit()
+	with open("log.txt", "a") as f:
+		f.write(f"Success {biden_count}-{trump_count} {datetime_string}\n")
+
+	driver.close()
+
+	driver.quit()
+
+
+if __name__ == "__main__":
+	update_colleges()
