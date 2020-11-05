aa983d26be286ef0ee56b73cc6e6c634bc9bbb27
Liam Ensbey <ensbeyliam@myvuw.ac.nz>
2020-11-05 14:16:31 +1300

    Better logging

--- main.py
+++ main.py
@@ -3,11 +3,22 @@ from selenium import webdriver
 import json
 from datetime import datetime
 import platform
+import os
+import logging
+import requests
+
+date_fmt = '%d/%m/%Y %H:%M:%S'
 
 
 def update_colleges():
 
-	datetime_string = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+	log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+
+	logging.basicConfig(filename='main.log', filemode='w', format=log_fmt, datefmt=date_fmt, level=logging.INFO)
+
+	source_dir = os.path.dirname(os.path.abspath(__file__))
+
+	logging.info(f'Source directory = {source_dir}')
 
 	options = webdriver.ChromeOptions()
 	options.add_argument('headless')
@@ -21,44 +32,74 @@ def update_colleges():
 	else:
 		driver = webdriver.Chrome(executable_path="/usr/local/bin/chromedriver", options=options)
 
+	logging.info(f'Set up chromedriver for {platform.system()} OS')
+
 	url = "https://www.theguardian.com/us-news/ng-interactive/2020/nov/03/us-election-2020-live-results-donald-trump-joe-biden-who-won-presidential-republican-democrat"
+
+	logging.info(f'Requesting HTTP from URL...')
+
+	resp = requests.get(url)
+
+	if resp.status_code == 200:
+		logging.info(f'Response status code {resp.status_code}')
+	else:
+		logging.error(f'Response status code {resp.status_code}')
+		write_to_log_list(False)
+		return
 
 	driver.get(url)
 
 	biden_xpath = '/html/body/div[4]/article/div/div[2]/div/figure/figure/div/div/div[1]/div/div[1]/div[1]/div[2]/div[1]/div[1]'
 	trump_xpath = '/html/body/div[4]/article/div/div[2]/div/figure/figure/div/div/div[1]/div/div[1]/div[2]/div[2]/div[2]/div[2]'
 
-	try:
-		biden_count = int(driver.find_element_by_xpath(biden_xpath).text.strip())
-		trump_count = int(driver.find_element_by_xpath(trump_xpath).text.strip())
-	except ValueError:
-		with open("log.txt", "a") as f:
-			f.write(f"Failure ValueError {datetime_string}\n")
+	biden_element = driver.find_element_by_xpath(biden_xpath)
+	trump_element = driver.find_element_by_xpath(trump_xpath)
+
+	if biden_element and trump_element:
+		try:
+			biden_count = int(biden_element.text.strip())
+			trump_count = int(trump_element.text.strip())
+		except ValueError:
+			logging.error(f'Cannot retrieve data from webpage element')
+			write_to_log_list(False)
 			return
+	else:
+		logging.error(f'Cannot retrieve element from webpage')
+		write_to_log_list(False)
+		return
 
-	if biden_count and trump_count:
+	data = {
+		"biden": {
+			"college_count": f"{biden_count}",
+			"width": f"{round(biden_count/270*50, 2)}%"
+		},
+		"trump": {
+			"college_count": f"{trump_count}",
+			"width": f"{round(trump_count/270*50, 2)}%"
+		},
+		"time_updated": datetime.now().strftime(date_fmt)
+	}
 
-		data = {
-			"biden": {
-				"college_count": f"{biden_count}",
-				"width": f"{round(biden_count/270*50, 2)}%"
-			},
-			"trump": {
-				"college_count": f"{trump_count}",
-				"width": f"{round(trump_count/270*50, 2)}%"
-			},
-			"time_updated": datetime.now().strftime("%d/%m/%Y %H:%M:%S")
-		}
+	with open(f"{source_dir}/public_html/us-election/data.json", "w") as f:
+		json.dump(data, f, indent=4)
 
-		with open("public_html/us-election/data.json", "w") as f:
-			json.dump(data, f, indent=4)
-
-	with open("log.txt", "a") as f:
-		f.write(f"Success {biden_count}-{trump_count} {datetime_string}\n")
+	write_to_log_list()
 
 	driver.close()
 
 	driver.quit()
+
+	logging.info(f'Successful.')
+
+
+def write_to_log_list(success=True):
+	if success:
+		success_failure = "success"
+	else:
+		success_failure = "failure"
+
+	with open(f"{os.path.dirname(os.path.abspath(__file__))}/list.log", "a") as f:
+		f.write(f"{success_failure} {datetime.now().strftime(date_fmt)}\n")
 
 
 if __name__ == "__main__":
