f8e2bf2660a41e3aed509d9fa0eb8bb548c89875
Liam Ensbey <ensbeyliam@myvuw.ac.nz>
2020-11-05 14:54:51 +1300

    Working on server, updated log files

--- main.py
+++ main.py
@@ -24,8 +24,8 @@ def update_colleges():
 	options.add_argument('headless')
 	options.add_argument('--no-sandbox')
 	options.add_argument('--disable-dev-shm-usage')
-	options.add_argument('--disable-gpu')
-	options.add_argument('--remote-debugging-port=9222')
+	# options.add_argument('--disable-gpu')
+	# options.add_argument('--remote-debugging-port=9222')
 
 	if platform.system() == "Windows":
 		driver = webdriver.Chrome(options=options)
